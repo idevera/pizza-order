196a797f3981f617283c0fd8886a7ef478a8d1df
Irene <irenneeyyy@gmail.com>
2017-04-02 20:04:39 -0700

    Add new method for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 //Business Logic
+//Object Constructor for size and toppings
 function Order(size, toppings){
   this.size = size;
   this.toppings = toppings;
 }
 
-//Prototype method to calculate the cost of the pizza
-Order.prototype.orderCost = function(){
+//Prototype method to calculate the cost of pizza size
+Order.prototype.costOfPizzaSize = function(){
   if (this.size === "small") {
     return 5;
+  } else if (this.size === "medium") {
+    return 10;
+  } else if (this.size === "large") {
+    return 15;
+  } else {
+    return 20;
   }
 }
 
-
+//Prototype method to calculate cost of toppings
+Order.prototype.costOfToppings = function(){
+  for(var index = 0; index <= this.toppings.length; index += 1) {
+    console.log(index);
+  }
+}
 
 //Interface Logic
 $(document).ready(function(){
@@ -29,13 +41,12 @@ $(document).ready(function(){
     //Create new object after user submits the form with selections
     var newOrder = new Order(inputPizzaSize, toppingsList);
 
-    console.log(newOrder.orderCost());
+    console.log(newOrder.costOfPizzaSize());
+    console.log(newOrder.costOfToppings());
 
     // $("#cost").show(newOrder.orderCost());
 
   });
 });
 
-
-//Object Constructor for toppings and size
 //Prototype method for the cost of a pizza!
