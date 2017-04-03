0e50ba3373208ccfde7591125968ffb120f39db2
Irene <irenneeyyy@gmail.com>
2017-04-02 18:00:46 -0700

    Add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 //Business Logic//
-function(size, toppings) {
-  
+function Order(size,toppings){
+  this.size = size;
+  this.toppings = toppings;
 }
+
+//Prototype method to calculate the cost of the pizza//
 
 
 //Interface Logic//
+$(document).ready(function(){
+  $("form#order-form").submit(function(event){
+    event.preventDefault();
+
+    var userOrder = new Order(size toppings);
+  });
+});
+
+//Create new object//
+
+
+//Object Constructor for toppings and size
+//Prototype method for the cost of a pizza!
